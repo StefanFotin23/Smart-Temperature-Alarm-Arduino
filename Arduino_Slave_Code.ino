c620a1927fe1cab80cb96d8b562968bf4f36db69
Stefan <andreifotin@gmail.com>
2023-05-26 16:10:37 +0300

    Update Arduino_Slave_Code.ino

--- Arduino_Slave_Code.ino
+++ Arduino_Slave_Code.ino
@@ -13,35 +13,35 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  	lcd.begin(16, 2);
-  	lcd.print("Buna ziua!");
-	Wire.begin(I2C_SLAVE_ADDRESS);
+    lcd.begin(16, 2);
+    lcd.print("Buna ziua!");
+  Wire.begin(I2C_SLAVE_ADDRESS);
 }
 
 void receiveEvent(int numBytes) {
-  	i = 3;
-  	temp = 0;
-  	while (0 <= i) {
-      	aux = Wire.read();
-      	if (aux > 127) {
-      		aux = aux - 256;
-      	}
-  		temp = temp + pow(10, i) * aux;
-      	i--;
+    i = 3;
+    temp = 0;
+    while (0 <= i) {
+        aux = Wire.read();
+        if (aux > 127) {
+          aux = aux - 256;
+        }
+      temp = temp + pow(10, i) * aux;
+        i--;
     }
 }
 
 void loop() {
-  	Wire.onReceive(receiveEvent);
-  	if (temp != 0) {
-      	lcd.clear();
-  		lcd.print("Temp refresh");
-  		delay(1000);
-      	lcd.clear();
-  		lcd.print(temp / 100);
-  		lcd.print(".");
-		lcd.print(abs(temp % 100));
-  		lcd.print(" grade C");
-      	delay(5000);
+    Wire.onReceive(receiveEvent);
+    if (temp != 0) {
+        lcd.clear();
+      lcd.print("Temp refresh");
+      delay(1000);
+        lcd.clear();
+      lcd.print(temp / 100);
+      lcd.print(".");
+    lcd.print(abs(temp % 100));
+      lcd.print(" grade C");
+        delay(5000);
     }
 }
